6630f01d7af60eb588dbcaa1ea07876a3a9f15e1
s4kr4 <s4kr4m4@gmail.com>
2025-02-23 21:21:59 +0900

    Fix query

--- src/server/api/routers/character.ts
+++ src/server/api/routers/character.ts
@@ -4,12 +4,7 @@ import { createTRPCRouter, publicProcedure } from "../trpc";
 export const characterRouter = createTRPCRouter({
   getAll: publicProcedure
     .query(async ({ ctx }) => {
-      const characters = await ctx.db.character.findMany({
-        orderBy: {
-          rarity: 'desc',
-          name_jp: 'asc',
-        },
-      });
+      const characters = await ctx.db.character.findMany();
       return characters;
     }),
 });
